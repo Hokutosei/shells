1b8d14da0a5af1491420f72c968a7b04fc9c3a72
H.Iwasaki <h.iwasaki@b-eee.com>
2017-09-19 16:33:31 +0900

    move oem files to shells-ix

--- oem/mongodb/do.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-make () {
-	docker build --tag=gcr.io/bizplatform-ix-production/mongodb:3.4.0 --no-cache .
-    gcloud docker -- push gcr.io/bizplatform-ix-production/mongodb:3.4.0
-}
-
-$*
--- oem/redis/do.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-make () {
-	docker build --tag=gcr.io/bizplatform-ix-production/redis:3.2.3-alpine --no-cache .
-    gcloud docker -- push gcr.io/bizplatform-ix-production/redis:3.2.3-alpine
-}
-
-$*
